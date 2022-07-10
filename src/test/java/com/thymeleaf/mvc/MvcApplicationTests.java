a48cd515efe8fde63343ff9d9bb8b0b20871e461
Jose Camarena <jose.j.camarena@gmail.com>
2022-07-09 22:03:51 -0700

    memoriesapplication

--- src/test/java/com/thymeleaf/mvc/MvcApplicationTests.java
+++ src/test/java/com/thymeleaf/mvc/MvcApplicationTests.java
@@ -1,13 +1,26 @@
 package com.thymeleaf.mvc;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.thymeleaf.mvc.controllers.MainController;
 
 @SpringBootTest
 class MvcApplicationTests {
+	@Autowired
+	private MainController mc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getIndex() {
+		String page = mc.index();
+		assertAll(() -> assertEquals("index", page));
 	}
 
 }
